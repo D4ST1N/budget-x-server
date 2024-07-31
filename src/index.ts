e0e9361b35913da950c27b7a5c09874b3fdd1cf4
D4ST1N <d4st1nh0uk@gmail.com>
2024-08-01 02:19:05 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
